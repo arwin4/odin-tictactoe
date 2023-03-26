f65c657676ad5972506eee9d9c21c79a521129a7
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-03-26 11:37:46 +0200

    Add win detection
    - Change empty cells of the board to null values, from '0'. 'null' is
      both more semantic and prevents '0' from being confused with O.
    - The board does not reset yet. This will be implemented in the DOM
      context.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//TODO: Disable no-console ESLINT
+//TODO: Add underscore to private variables
+
 const playerFactory = (name, marker) => {
   const getName = () => name;
   const getMarker = () => marker;
@@ -7,12 +10,12 @@ const playerFactory = (name, marker) => {
 
 const gameBoard = (() => {
   // Initialize empty board, a two-dimensional array
-  // '.' = blank cell.
+  // null = blank cell.
   const gameBoardArray = [];
   for (let i = 0; i < 3; i += 1) {
     gameBoardArray[i] = [];
     for (let j = 0; j < 3; j += 1) {
-      gameBoardArray[i].push('.');
+      gameBoardArray[i].push(null);
     }
   }
 
@@ -25,7 +28,7 @@ const gameBoard = (() => {
       return false;
 
     // Place marker if cell is blank
-    if (gameBoardArray[y - 1][x - 1] === '.') {
+    if (gameBoardArray[y - 1][x - 1] === null) {
       gameBoardArray[y - 1][x - 1] = marker;
       // also return checkForWin(marker)
       return true;
@@ -33,9 +36,54 @@ const gameBoard = (() => {
     return false;
   }
 
-  // checkForWin(marker)
-  // resetBoard()
-  // getGameboard
+  function checkForWin(marker) {
+    // Check for a win first. If there's none, check for a draw.
+    // NOTE: I realize it's faster to check against a list of all win
+    // conditions, since that's a fairly short one, but to practice, I wanted to
+    // write down more of the actual logic of the game.
+
+    // Check for horizontal wins
+    for (let i = 0; i < 3; i += 1) {
+      const row = gameBoardArray[i];
+      if (row.every((cell) => cell === marker)) {
+        return true;
+      }
+    }
+
+    // Check for vertical wins
+    // TODO: find wins like horizontal using every()
+    for (let i = 0; i < 3; i += 1) {
+      if (
+        gameBoardArray[0][i] === marker &&
+        gameBoardArray[1][i] === marker &&
+        gameBoardArray[2][i] === marker
+      ) {
+        return true;
+      }
+    }
+
+    // Check for diagonal wins
+    if (
+      (gameBoardArray[0][0] === marker &&
+        gameBoardArray[1][1] === marker &&
+        gameBoardArray[2][2] === marker) ||
+      (gameBoardArray[0][2] === marker &&
+        gameBoardArray[1][1] === marker &&
+        gameBoardArray[2][0] === marker)
+    ) {
+      return true;
+    }
+
+    // If there's no winner but the board is full, it's a draw.
+    // Check whether every cell isn't still null.
+    if (gameBoardArray.every((row) => row.every((cell) => cell !== null))) {
+      return false;
+    }
+
+    // No game ending state found
+    return undefined;
+  }
+
   const getGameBoard = () => gameBoardArray;
 
   function printGameBoard() {
@@ -44,7 +92,7 @@ const gameBoard = (() => {
     console.log(gameBoardArray[2]);
   }
 
-  return { getGameBoard, makeMove, printGameBoard };
+  return { getGameBoard, makeMove, printGameBoard, checkForWin };
 })();
 
 const gameController = (() => {
@@ -87,7 +135,7 @@ const gameController = (() => {
 
   function printNewRound() {
     gameBoard.printGameBoard();
-    console.log(`It's ${activePlayer.getName()}'s turn.`);
+    // console.log(`It's ${activePlayer.getName()}'s turn.`);
   }
 
   function playRound(x, y) {
@@ -98,10 +146,27 @@ const gameController = (() => {
       return;
     }
 
-    // checkForWin
-    console.log(`${activePlayer.getName()} made a move:`);
-    switchPlayerTurn();
+    // Check for a win or draw
+    switch (gameBoard.checkForWin(activePlayer.getMarker())) {
+      case true:
+        console.log(`${activePlayer.getName()} wins!`);
+        // resetBoard();
+        break;
+      case false:
+        console.log("It's a draw.");
+        // resetBoard();
+        break;
+      case undefined:
+        console.log('No win or draw detected');
+        // No win or draw detected, game continues.
+        break;
+      default:
+        break;
+    }
+
+    console.log(`${activePlayer.getName()} made this move:`);
     printNewRound();
+    switchPlayerTurn();
   }
 
   printNewRound();
